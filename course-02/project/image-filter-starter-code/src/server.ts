a37cf0abb64936479cf2f42e1b1c09d46b4c450e
Joao Ricardi <joaoricardi@MacBook-Pro-de-Joao.local>
2022-05-08 20:35:47 -0300

    add tipagem do typescript

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -13,7 +13,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get("/filteredimage", async (req, res) => {
+  app.get("/filteredimage", async (req:express.Request, res:express.Response) => {
     try{
       let {image_url} = req.query;
       if(!image_url){
